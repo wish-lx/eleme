56c1fd83f8bb36e3e091fb373ba24d9ad93c70b8
zhaolingxue <18222478537@163.com>
2018-03-29 11:21:24 +0800

    '商家公告'

--- src/components/seller/seller.vue
+++ src/components/seller/seller.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="seller">
+  <div class="seller" ref="seller">
     <div class="seller-content">
       <div class="overview">
         <h1 class="title">{{seller.name}}</h1>
@@ -42,10 +42,22 @@
                   </li>
               </ul>
       </div>
+      <split></split>
+      <div class="pics">
+        <h1 class="title">商家实景</h1>
+        <div class="pic-wrapper">
+          <ul class="pic-list">
+            <li class="pic-item" v-for="pic in seller.pics" >
+              <img :src="pic" width="120" height="90" alt="">
+            </li>
+          </ul>
+        </div>
+      </div>
     </div>
   </div>
 </template>
 <script type="text/ecmascript-6" >
+ import BScroll from 'better-scroll'
  import star from 'components/star/star'
  import split from 'components/split/split'
   export default {
@@ -57,6 +69,25 @@
     },
     created () {
        this.classMap = [ 'decrease', 'discount', 'special', 'invoice', 'guarantee' ]
+    },
+    watch: {
+      'seller' () {
+        this._initScroll()
+      }
+    },
+    ready () {
+      this._initScroll()
+    },
+    methods: {
+       _initScroll () {
+         if (!this.scroll) {
+           this.scroll = new BScroll(this.$refs.seller, {
+             click: true
+           })
+         } else {
+           this.scroll.refresh()
+         }
+       }
     },
     components: {
       star,
@@ -134,6 +165,8 @@
           padding: 16px 12px 
           border-1px(rgba(7,17,27,0.1))
           font-size: 0
+          &:last-child
+             border-none()
         .icon 
           display:inline-block 
           width:16px 
@@ -156,6 +189,27 @@
           line-height: 16px 
           font-size: 12px 
           color: rgb(7,17,27)    
+    .pics
+      padding: 18px 
+      .title
+        margin-bottom: 12px 
+        line-height: 14px 
+        color: rgb(7,17,27) 
+        font-size: 14px 
+      .pic-wrapper 
+        width: 100% 
+        overflow: hidden 
+        white-space: nowrap 
+        .pic-list
+          font-size:0 
+          .pic-item 
+            display: inline-block 
+            margin-right: 6px 
+            width: 120px 
+            height: 90px 
+            &:last-child 
+              margin: 0  
 
+      
         
 </style>
